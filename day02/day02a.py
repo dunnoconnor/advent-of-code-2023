c7a290dc254f175929266641eb2db164010ad6d5
dunnoconnor <michael.dunn.oconnor@gmail.com>
2023-12-02 06:51:26 -0500

    day 2 plan

---
+++ day02/day02a.py
@@ -0,0 +1,9 @@
+
+# store max values for each color
+# read text file, splitting by line
+# split line into id value, r val, g val, b val
+# if r,g,and b all below limits - add id to sum
+# return sum
+
+def get_possible_games(text):
+    pass
