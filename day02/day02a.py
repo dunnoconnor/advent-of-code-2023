b4a3a8c90276b426efbba84b5bcc9063f6b8a99c
dunnoconnor <michael.dunn.oconnor@gmail.com>
2023-12-02 09:38:42 -0500

    day02a

--- day02/day02a.py
+++ day02/day02a.py
@@ -1,9 +1,41 @@
-
-# store max values for each color
-# read text file, splitting by line
+from parse import *
 # split line into id value, r val, g val, b val
 # if r,g,and b all below limits - add id to sum
 # return sum
 
-def get_possible_games(text):
-    pass
+# open file, read data, split by newline
+f = open("day02.data.txt", "r");
+text = f.read();
+f.close();
+
+# store max values for each color
+limits = {"red":12,"green":13,"blue":14};
+
+def get_possible_games(text:str) -> int:
+    id_total = 0;
+    lines = text.split("\n")
+    # split line into id value, r val, g val, b val
+    for l in lines:
+        l = l.split(":");
+        game = l.pop(0);
+        game = parse('Game {:d}', game)[0];
+        rounds = l[0].split(";");
+        id_total += check_game(game,rounds);
+        print(game, id_total)
+
+    return id_total;
+
+def check_game(game:int, rounds:list) -> int:
+    for pull in rounds:
+        pull = pull.split(",");
+        for die in pull:
+            die = die.strip();
+            p = parse('{:d} {:w}', die)
+            num=p[0]
+            color=p[1]
+            if(num>limits[color]):
+                print(num, color, " is too many")
+                return 0;
+    return game;
+
+print(get_possible_games(text));
---
+++ day02/day02a.test.py
@@ -0,0 +1,14 @@
+import unittest
+from day02.day02a import get_possible_games
+
+# open file, read data, split by newline
+f = open("day01.sample.txt", "r");
+text = f.read();
+f.close();
+
+class TestDay01(unittest.TestCase):
+    def test_a(self):
+        self.assertEqual(get_possible_games(text),8)
+
+if __name__ == '__main__':
+    unittest.main()
